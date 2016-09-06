44582a28a54b79676d94db0231db558dbf4ecafb
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-06 20:56:44 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,47 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class positive
+{
+	
+
+    public static void main(String[] args) 
+
+    {
+
+        int n;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the number :");
+
+        n = s.nextInt();
+
+        if(n > 0)
+
+        {
+
+            System.out.println("The given number "+n+" is Positive");
+
+        }
+
+        else if(n < 0)
+
+        {
+
+            System.out.println("The given number "+n+" is Negative");
+
+        }
+
+        else
+
+        {
+
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+
+        }
+
+    }
+
+}
